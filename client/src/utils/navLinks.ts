38c485ab65448c3ad551b5b3adcf7968135bceb5
Ckm54 <mutaic84@gmail.com>
2023-05-31 17:13:58 +0300

    create navigation template

---
+++ client/src/utils/navLinks.ts
@@ -0,0 +1,12 @@
+import { NavItem } from "../interfaces/NavItem";
+
+export const navLinks: NavItem[] = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "Products",
+    href: "/products",
+  },
+];
