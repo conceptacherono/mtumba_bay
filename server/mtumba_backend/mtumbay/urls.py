5ab1359f8cfaf0633b3c93191f60bff0bfb90f14
conceptacherono <91899034+conceptacherono@users.noreply.github.com>
2023-06-05 14:49:13 +0300

    order enpoints
    order endpoints

--- server/mtumba_backend/mtumbay/urls.py
+++ server/mtumba_backend/mtumbay/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
-from .views import UserRegistrationView, UserLogoutView, UserProfileView
+from .views import UserRegistrationView, UserLogoutView, UserProfileView, create_order, order_details, order_history, cancel_order, order_tracking
 from rest_framework.authtoken.views import obtain_auth_token
 
 app_name = 'mtumbay'
 
 urlpatterns = [
+    #user endpoints
     path('api/auth/register', UserRegistrationView.as_view(), name='register'),
     path('api/auth/login', obtain_auth_token, name='login'),
     path('api/auth/logout', UserLogoutView.as_view(), name='logout'),
-    path('api/auth/profile', UserProfileView.as_view(), name='profile')
+    path('api/auth/profile', UserProfileView.as_view(), name='profile'),
+    
+    #order enpoints
+    path('api/orders/', create_order, name='create_order'),
+    path('api/orders/<str:order_id>/', order_details, name='order_details'),
+    path('api/users/<str:user_id>/orders/', order_history, name='order_history'),
+    path('api/orders/<str:order_id>/cancel/', cancel_order, name='cancel_order'),
+    path('api/orders/<str:order_id>/tracking/', order_tracking, name='order_tracking'),
 ]
+
--- server/mtumba_backend/mtumbay/views.py
+++ server/mtumba_backend/mtumbay/views.py
@@ -9,7 +9,9 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .serializers import OrderDetailsSerializer, OrderHistorySerializer, OrderSerializer, OrderTrackingSerializer
 
+    #user endpoints
 class UserRegistrationView(APIView):
     authentication_classes = []
     permission_classes = []
@@ -38,3 +40,63 @@ class UserProfileView(APIView):
      def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)  
+     
+     #order-endpoints
+@api_view(['POST'])
+def create_order(request):
+    serializer = OrderSerializer(data=request.data)
+    if serializer.is_valid():
+        # Perform order creation logic here
+        order_id = "67890"  # Generated order ID
+        response_data = {
+            "order_id": order_id,
+            "status": "created",
+            "message": "Order created successfully."
+        }
+        return Response(response_data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def order_details(request, order_id):
+    # Retrieve order details for the given order ID
+    # Perform logic to fetch order details
+    order = {
+        "order_id": order_id,
+        # Other order details
+    }
+    serializer = OrderDetailsSerializer(order)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def order_history(request, user_id):
+    # Retrieve order history for the given user ID
+    # Perform logic to fetch order history
+    orders = [
+        {
+            "order_id": "123",
+            # Other order details
+        },
+        {
+            "order_id": "456",
+            # Other order details
+        },
+    ]
+    serializer = OrderHistorySerializer(orders, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def cancel_order(request, order_id):
+    # Logic to cancel the order with the given order ID
+    # Perform cancellation logic
+    return Response({"message": "Order canceled successfully."})
+
+@api_view(['GET'])
+def order_tracking(request, order_id):
+    # Retrieve tracking information for the given order ID
+    # Perform logic to fetch tracking information
+    tracking_info = {
+        "order_id": order_id,
+        # Other tracking information
+    }
+    serializer = OrderTrackingSerializer(tracking_info)
+    return Response(serializer.data)     
