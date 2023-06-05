488f60e5bd0c9976380a1133ce43c40882e7757c
conceptacherono <91899034+conceptacherono@users.noreply.github.com>
2023-06-05 15:11:19 +0300

    product enpoints
    product endpoints

--- server/mtumba_backend/mtumbay/models.py
+++ server/mtumba_backend/mtumbay/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+  #product enpoints
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+     
+  #review 
 class Review(models.Model):
     userId = models.CharField(max_length=100)
     rating = models.FloatField()
