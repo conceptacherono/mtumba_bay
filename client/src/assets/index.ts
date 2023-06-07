f747e31b8827fd36e284030bc377cd87719090d1
Ckm54 <mutaic84@gmail.com>
2023-06-07 08:59:52 +0300

    update shopping categories section

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -3,5 +3,6 @@ import Travel1 from "./travel-1.jpg";
 import Travel2 from "./travel-2.jpg";
 import Clothing from "./clothing.jpg";
 import Sneakers from "./sneakers.jpg";
+import Tech from "./tech.jpg";
 
-export { Mens, Travel1, Travel2, Clothing, Sneakers };
+export { Mens, Travel1, Travel2, Clothing, Sneakers, Tech };
--- client/src/components/productCategories/index.tsx
+++ client/src/components/productCategories/index.tsx
@@ -1,4 +1,4 @@
-import { Mens, Travel1, Clothing, Sneakers, Travel2 } from "../../assets";
+import { Mens, Travel1, Clothing, Sneakers, Travel2, Tech } from "../../assets";
 import { ProductCategoryType } from "../../interfaces/ProductCategory";
 import CategoryCard from "./CategoryCard";
 
@@ -7,7 +7,7 @@ const ProductCategories = () => {
     <section className="py-24">
       <h2 className="font-[500] text-2xl">Shop our top categories</h2>
 
-      <div className="flex gap-4">
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
         {productCategoryData.map((category) => (
           <CategoryCard key={category.index} category={category} />
         ))}
@@ -31,6 +31,11 @@ const productCategoryData: ProductCategoryType[] = [
     index: 3,
     name: "Travel",
     image: Travel1,
+  },
+  {
+    index: 4,
+    name: "Tech",
+    image: Tech,
   },
   {
     index: 4,
