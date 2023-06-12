6f177e70cfee43051fa9d9cbd4424ef28f0bc465
Ckm54 <mutaic84@gmail.com>
2023-06-12 17:03:47 +0300

    product details page scaffold

---
+++ client/src/components/products/ProductDetails.tsx
@@ -0,0 +1,9 @@
+const ProductDetails = () => {
+  return (
+    <div className="h-full w-full flex items-center justify-center">
+      ProductDetails page
+    </div>
+  );
+};
+
+export default ProductDetails;
