bee6418da4731aa3e2e92eec119ca6cc947b3774
Ckm54 <mutaic84@gmail.com>
2023-06-12 17:04:04 +0300

    add link to details page

--- client/src/lib/ui/ProductCard.tsx
+++ client/src/lib/ui/ProductCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { ProductType } from "../../interfaces/Product";
 
 interface ProductCardProps {
@@ -7,12 +8,20 @@ interface ProductCardProps {
 const ProductCard = ({ product }: ProductCardProps) => {
   return (
     <div>
-      <div className="h-72 flex items-center justify-center">
+      <div className="relative h-72 flex items-center justify-center cursor-pointer group p-8">
         <img
           src={product.image}
           alt={product.title}
           className=" h-full object-contain transition hover:scale-105"
         />
+        <div className="absolute top-0 left-0 w-full h-0 flex flex-col justify-center items-center bg-opacity-20 bg-black opacity-0 group-hover:h-full group-hover:opacity-100 duration-300">
+          <Link
+            className="mt-5 px-8 py-3 rounded-full bg-amber-400 hover:bg-amber-600 duration-300"
+            to={`/products/${product.id}`}
+          >
+            View Details
+          </Link>
+        </div>
       </div>
       <div className="px-4">
         <div className="flex py-2 mt-2">
