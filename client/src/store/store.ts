c627aa0054c9b7959e085274fd4b4db2e41a3c13
Collins <39172663+Ckm54@users.noreply.github.com>
2023-06-13 15:41:25 +0300

    Update store.ts
    remove duplicate productSlice import

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -2,7 +2,6 @@ import { configureStore} from "@reduxjs/toolkit";
 import ProductSlice from "./features/ProductSlice";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { productsApi } from "../services/productsApi";
-import ProductSlice from "./features/ProductSlice";
 
 export const store = configureStore({
   reducer: {
