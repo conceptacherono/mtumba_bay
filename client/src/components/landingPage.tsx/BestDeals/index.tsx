d9df68db69ace6624dc3af048426c82253b84f03
Ckm54 <mutaic84@gmail.com>
2023-06-16 17:23:12 +0300

    handle displaying products by category button click

--- client/src/components/landingPage.tsx/BestDeals/index.tsx
+++ client/src/components/landingPage.tsx/BestDeals/index.tsx
@@ -5,14 +5,20 @@ import {
   useGetProductsQuery,
 } from "../../../services/productsApi";
 import Loader from "../../Loader";
+import { useDispatch } from "react-redux";
 
 const BestDeals = () => {
+  const dispatch = useDispatch();
+  const [activeCategory, setActiveCategory] = React.useState<string>("all");
+
   const { data, error, isLoading, isFetching, isSuccess } =
-    useGetProductsQuery();
+    useGetProductsQuery(activeCategory);
   const { data: categories, isLoading: loadingCategories } =
     useGetCategoriesQuery();
 
-  const [activeCategory, setActiveCategory] = React.useState<string>("all");
+  const handleCategoryChange = (category: string) => {
+    setActiveCategory(category);
+  };
 
   return (
     <section className="pt-16">
@@ -34,7 +40,7 @@ const BestDeals = () => {
               activeCategory === category &&
               "bg-darkGreen border-darkGreen text-white"
             }`}
-            onClick={() => setActiveCategory(category)}
+            onClick={() => handleCategoryChange(category)}
           >
             {category}
           </div>
