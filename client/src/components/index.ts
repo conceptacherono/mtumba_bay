03d523c95fee613f0dfaf76b6e17f045060889a4
Ckm54 <mutaic84@gmail.com>
2023-06-16 18:20:34 +0300

    update barrel file

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,6 +1,16 @@
 import LandingPage from "./landingPage.tsx/LandingPage";
 import Products from "./products/Products.tsx";
 import ProductDetails from "./products/ProductDetails.tsx";
+import Cart from "./Cart/index.tsx";
+import ErrorPage from "./ErrorPage/index.tsx";
 import { SignUP, Login } from "./auth/index.ts";
 
-export { Products, LandingPage, ProductDetails, SignUP, Login };
+export {
+  Products,
+  LandingPage,
+  ProductDetails,
+  SignUP,
+  Login,
+  Cart,
+  ErrorPage,
+};
