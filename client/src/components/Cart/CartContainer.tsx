efe26f7ed89c43db564870d026344808cd40a318
Ckm54 <mutaic84@gmail.com>
2023-06-18 00:14:49 +0300

    add order info section

--- client/src/components/Cart/CartContainer.tsx
+++ client/src/components/Cart/CartContainer.tsx
@@ -1,5 +1,5 @@
 import { ProductType } from "../../interfaces/Product";
-import { CartItem } from "../../lib";
+import { CartItem, CartSummary, OrderInformation } from "../../lib";
 
 type Props = {
   productsInCart: ProductType[];
@@ -13,9 +13,12 @@ const CartContainer = ({ productsInCart }: Props) => {
           {productsInCart.map((product) => (
             <CartItem product={product} key={product.id} />
           ))}
+
+          <OrderInformation />
         </div>
         <div className="flex-1">
           <h2 className="font-bold text-2xl">Order Summary</h2>
+          <CartSummary />
         </div>
       </div>
     </div>
