aad653217e9a69f18d33fbdbc1b62b8f99c555f7
Ckm54 <mutaic84@gmail.com>
2023-06-17 23:28:01 +0300

    add methods to update cart states

--- client/src/store/features/ProductSlice.ts
+++ client/src/store/features/ProductSlice.ts
@@ -24,14 +24,25 @@ export const ProductSlice = createSlice({
     addProductToCart: (state, action: PayloadAction<ProductType>) => {
       state.cart.push(action.payload);
     },
-    // todo:: Update filter logic
+    updateCartProducts: (state, action: PayloadAction<ProductType>) => {
+      state.cart = state.cart.map((product) => {
+        if (product.id === action.payload.id) {
+          console.log("here", action.payload);
+          return action.payload;
+        } else {
+          return product;
+        }
+      });
+    },
+    ///// todo:: Update filter logic
     removeProductFromCart: (state, action: PayloadAction<ProductType>) => {
       state.cart = state.cart.filter(
-        (product) => product.quantity !== action.payload.quantity
+        (product) => product.id !== action.payload.id
       );
     },
   },
 });
 
 export default ProductSlice;
-export const { addProductToCart, removeProductFromCart } = ProductSlice.actions;
+export const { addProductToCart, updateCartProducts, removeProductFromCart } =
+  ProductSlice.actions;
