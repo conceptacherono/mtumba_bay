78a9335888e6e89526588ac9a7d74331d6751f65
Ckm54 <mutaic84@gmail.com>
2023-06-18 01:05:17 +0300

    update type definition

--- client/src/interfaces/Product.ts
+++ client/src/interfaces/Product.ts
@@ -3,7 +3,7 @@ export interface ProductType {
   title: string;
   description: string;
   category: string;
-  quantity?: number;
+  quantity: number | 1;
   price: number;
   image: string;
   rating: {
