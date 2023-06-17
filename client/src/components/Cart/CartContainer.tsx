ec007bab3a26961a903726e16cb13d6df9256996
Ckm54 <mutaic84@gmail.com>
2023-06-18 01:04:31 +0300

    update render order

--- client/src/components/Cart/CartContainer.tsx
+++ client/src/components/Cart/CartContainer.tsx
@@ -8,19 +8,18 @@ type Props = {
 const CartContainer = ({ productsInCart }: Props) => {
   return (
     <div>
-      <div className="flex gap-4">
+      <div className="flex flex-col md:flex-row gap-4">
         <div className="flex-1">
           {productsInCart.map((product) => (
             <CartItem product={product} key={product.id} />
           ))}
-
-          <OrderInformation />
         </div>
         <div className="flex-1">
           <h2 className="font-bold text-2xl">Order Summary</h2>
           <CartSummary />
         </div>
       </div>
+      <OrderInformation />
     </div>
   );
 };
