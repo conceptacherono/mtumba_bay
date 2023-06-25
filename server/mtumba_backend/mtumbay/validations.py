6a121af953a8299299669ec6ba9bc067d7411169
conceptacherono <91899034+conceptacherono@users.noreply.github.com>
2023-06-25 15:33:39 +0300

    Update validations.py

--- server/mtumba_backend/mtumbay/validations.py
+++ server/mtumba_backend/mtumbay/validations.py
@@ -3,9 +3,9 @@ from django.contrib.auth import get_user_model
 UserModel = get_user_model()
 
 def custom_validation(data):
-    email = data['email'].strip()
-    username = data['username'].strip()
-    password = data['password'].strip()
+    email = data.get['email'].strip()
+    username = data.get['username'].strip()
+    password = data.get['password'].strip()
     ##
     if not email or UserModel.objects.filter(email=email).exists():
         raise ValidationError('choose another email')
