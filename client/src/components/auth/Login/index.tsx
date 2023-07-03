ea78b584c87246260bc5c7ee132d25e050da86d3
Ckm54 <mutaic84@gmail.com>
2023-07-03 14:28:17 +0300

    component cleanup

--- client/src/components/auth/Login/index.tsx
+++ client/src/components/auth/Login/index.tsx
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
-import { loginUser } from '../../../api/auth';
-import axios from 'axios';
+import axios from "axios";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { loginUser } from "../../../api/auth";
 
-axios.defaults.xsrfCookieName = 'csrftoken';
-axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.xsrfHeaderName = "X-CSRFToken";
 axios.defaults.withCredentials = true;
 
 const Login: React.FC = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -16,10 +17,10 @@ const Login: React.FC = () => {
     try {
       const userData = { email, password };
       const response = await loginUser(userData);
-      console.log('Login successful!', response);
+      console.log("Login successful!", response);
       // Add any success handling logic here, such as redirecting to another page
     } catch (error) {
-      console.error('Login failed!', error);
+      console.error("Login failed!", error);
       // Add error handling logic here, such as displaying an error message
     }
   };
@@ -28,13 +29,25 @@ const Login: React.FC = () => {
     <form onSubmit={handleSubmit}>
       <label>
         Email:
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </label>
       <label>
         Password:
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </label>
       <button type="submit">Login</button>
+
+      <p>
+        Dont have an account? <Link to={"/register"}>Register</Link>
+      </p>
     </form>
   );
 };
